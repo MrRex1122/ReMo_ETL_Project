c77b8c6a45b1aebb82fb1cada763bdb1b52ba398
Evgenii Korolev <evgenii.korolev.sys@gmail.com>
2026-03-17 01:47:46 +1100

    Raise default matcher parallelism to ten

--- tests/test_config_paths.py
+++ tests/test_config_paths.py
@@ -73,9 +73,9 @@ class ConfigPathTests(unittest.TestCase):
             config.DEFAULT_MATCHER_PARALLEL_REQUESTS,
         )
 
-    def test_matcher_parallel_requests_default_is_six(self):
+    def test_matcher_parallel_requests_default_is_ten(self):
         os.environ.pop("REMO_MATCHER_PARALLEL_REQUESTS", None)
-        self.assertEqual(config.get_matcher_parallel_requests(), 6)
+        self.assertEqual(config.get_matcher_parallel_requests(), 10)
 
     def test_matcher_cache_db_path_uses_env_override(self):
         os.environ["REMO_MATCHER_CACHE_DB"] = "cache/custom.db"
